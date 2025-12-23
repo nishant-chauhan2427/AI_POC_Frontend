6c8dd8db4e912b705ff73154f4f7f427cc3faf34
nishant-chauhan2427 <nishant.chauhan@vayuz.com>
2025-12-23 17:41:44 +0530

    push 1

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 // export const API_BASE = "https://rex.vayuz.com/ai-interview/api/v1";
-const API_BASE=import.meta.env.API_BASE_URL
+const API_BASE=import.meta.env.VITE_API_BASE_URL
 
 // export const API_BASE = "http://localhost:8000/ai-interview/api/v1";
 
