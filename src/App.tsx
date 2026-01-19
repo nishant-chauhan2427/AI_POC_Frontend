4f559c092a96c95c94ad393785cceefb0d01829f
Priya <priya.katiyar@vayuz.com>
2026-01-19 11:55:21 +0530

    change state

--- src/App.tsx
+++ src/App.tsx
@@ -95,7 +95,7 @@ const mockQuestions: Record<string, QuestionData[]> = {
 };
 
 export default function App() {
-  const [currentStep, setCurrentStep] = useState(6);
+  const [currentStep, setCurrentStep] = useState(1);
 
   /* ---------------- URL DATA ---------------- */
   const [initialUserData, setInitialUserData] = useState(null);
